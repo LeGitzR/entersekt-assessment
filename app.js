abf28e156c9adf33fac1811dcf14892d0708ffac
H Linde <18168965@sun.ac.za>
2017-09-16 11:51:08 +0200

    Added edit functionality for todo list

--- app.js
+++ app.js
@@ -5,16 +5,18 @@ const urlencodedParser = bodyParser.urlencoded({ extended: false });
 const app = express();
 
 let todolist = [];
+let editVal = "";
 
 /* The to do list and the form are displayed */
 app.get('/todo', function(req, res) {
-    res.render('todo.ejs', { todolist, clickHandler:"func1();" });
+    res.render('todo.ejs', { todolist, clickHandler:"func1();", editVal });
 })
 
 /* Adding an item to the to do list */
 .post('/todo/add/', urlencodedParser, function(req, res) {
     if (req.body.newtodo != '') {
         todolist.push(req.body.newtodo);
+        editVal = "";
     }
     res.redirect('/todo');
 })
@@ -26,6 +28,17 @@ app.get('/todo', function(req, res) {
     }
     res.redirect('/todo');
 })
+
+/* Edits an item from the to do list by removing it from the todolist array
+   and placing the value in the text box to be edited and added again */
+.get('/todo/edit/:id', function(req, res) {
+    if (req.params.id != '') {
+        editVal = todolist[req.params.id];
+        todolist.splice(req.params.id, 1);
+    }
+    res.redirect('/todo');
+})
+
 
 /* Redirects to the to do list if the page requested is not found */
 .use(function(req, res, next){
